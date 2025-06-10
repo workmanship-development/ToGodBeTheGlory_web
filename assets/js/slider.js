d8a227b45b0caa72575e09d581e92cad502a4a29
developer_main <workmanship.co.dev@proton.me>
2025-06-10 16:53:49 -0500

    TGBTG - adding javascript for actual scroll of quote elements

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,1 +1,30 @@
 /* TGBTG - slider js, based off reference: https://www.w3schools.com/howto/howto_js_quotes_slideshow.asp */
+
+var slideIndex = 1;
+showsSlides(slideIndex);
+
+function plusSlides(n) {
+    showsSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showsSlides(slideIndex = n);
+}
+
+function showsSlides(n) {
+   console.log("here inside function")
+    var i;
+    var slides = document.getElementsByClassName("quotesSlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+
+}
